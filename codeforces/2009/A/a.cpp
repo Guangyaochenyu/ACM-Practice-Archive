25714fde6e90d7057cb878a6ab1628e46b0cea76
Guangyaochenyu <Guangyaochenyu@163.com>
2024-12-11 15:24:06 +0800

    CodeForces 2009 A

---
+++ codeforces/2009/A/a.cpp
@@ -0,0 +1,24 @@
+// CodeForces 2009 A
+// Guangyaochenyu
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string str;
+
+int solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << b - a << endl;
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  int T = 1;
+  cin >> T;
+  while (T--) solve();
+  return 0;
+}
