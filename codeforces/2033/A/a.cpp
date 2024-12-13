13ee8a2aec2e2b958df0cc251cfa8c4fe3347239
Guangyaochenyu <Guangyaochenyu@163.com>
2024-12-13 21:15:27 +0800

    CodeForces 2033 A

---
+++ codeforces/2033/A/a.cpp
@@ -0,0 +1,42 @@
+// CodeForces 2033 A
+// Guangyaochenyu
+
+//------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+//------------------------------------------------------------------------------
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef string str;
+//------------------------------------------------------------------------------
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define input(v) { for ( auto& x: v ) { cin >> x; } }
+#define output(v) { for ( auto x: v ) { cout << x << ' '; } cout << endl; }
+//------------------------------------------------------------------------------
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+//------------------------------------------------------------------------------
+
+
+int solve() {
+  int n;  cin >> n;
+  cout << ( n % 2 == 1 ? "Kosuke" : "Sakurako" ) << endl;
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  int T = 1;
+  cin >> T;
+  while (T--) solve();
+  return 0;
+}
