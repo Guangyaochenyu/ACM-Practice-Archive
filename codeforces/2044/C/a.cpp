f460d96cff13d5cac37e911927a6552238a86335
Guangyaochenyu <Guangyaochenyu@163.com>
2024-12-21 12:56:09 +0800

    CodeForces 2044 C

---
+++ codeforces/2044/C/a.cpp
@@ -0,0 +1,48 @@
+// CodeForces 2044 C
+// Guangyaochenyu
+
+//------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+//------------------------------------------------------------------------------
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef string str;
+//------------------------------------------------------------------------------
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define input(v) { for ( auto& x: v ) { cin >> x; } }
+#define output(v) { for ( auto x: v ) { cout << x << ' '; } cout << endl; }
+//------------------------------------------------------------------------------
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef priority_queue<int> pqi;
+//------------------------------------------------------------------------------
+
+
+
+int solve() {
+  int m, a, b, c, ma, mb, mc;
+  cin >> m >> a >> b >> c;
+  ma = min(m, a);
+  mb = min(m, b);
+  mc = min(c, (m-ma)+(m-mb));
+  cout << ma + mb + mc << endl;
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  int T = 1;
+  cin >> T;
+  while (T--) solve();
+  return 0;
+}
